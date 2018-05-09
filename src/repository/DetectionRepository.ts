2e3e606ba2a41236294ad83948f664ebd47e1554
rcaceiro <r.caceiro@hotmail.com>
2018-05-09 12:22:33 +0100

    implemented get detections on repository

--- src/repository/DetectionRepository.ts
+++ src/repository/DetectionRepository.ts
@@ -1,10 +1,28 @@
-import {EntityRepository, getConnection, Repository} from "typeorm";
+import {Between, EntityRepository, LessThan, MoreThan, Repository} from "typeorm";
 import {Detection} from "../entity/Detection";
-import {DetectionObject} from "../entity/DetectionObject";
 
 @EntityRepository(Detection)
-export class DetectionRepository extends Repository<Detection>{
+export class DetectionRepository extends Repository<Detection>
+{
+ public async get(startDate: Date, endDate: Date): Promise<Detection[]>
+ {
+  let options = {};
+  let hasStartDate=startDate !== undefined && startDate !== null;
+  let hasEndDate=endDate !== undefined && endDate !== null;
 
- 
+  if(hasStartDate&&hasEndDate)
+  {
+   options['date'] = Between(startDate,endDate);
+  }
+  else if (hasStartDate)
+  {
+   options['date'] = MoreThan(startDate);
+  }
+  else if(hasEndDate)
+  {
+   options['date'] = LessThan(endDate);
+  }
 
+  return await this.find(options);
+ }
 }
