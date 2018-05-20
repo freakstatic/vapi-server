f7eb2b09bc2f103a3cfcf7dd8880f38474178d3a
rcaceiro <r.caceiro@hotmail.com>
2018-05-20 13:52:44 +0100

    new custom function to get weekday name from date object

--- angular/src/utils/date.extensions.ts
+++ angular/src/utils/date.extensions.ts
@@ -4,15 +4,30 @@ declare global
 {
  interface Date
  {
-  toDateISOString(): string;
+  toDateISOString(): string,
+  getDayName(): string
  }
 }
 
-Date.prototype.toDateISOString = function(): string
+Date.prototype.toDateISOString = function (): string
 {
  return this.getFullYear() +
   '-' + pad(this.getMonth() + 1) +
   '-' + pad(this.getDate());
+};
+
+Date.prototype.getDayName = function (): string
+{
+ let weekday = new Array(7);
+ weekday[0] = "Sunday";
+ weekday[1] = "Monday";
+ weekday[2] = "Tuesday";
+ weekday[3] = "Wednesday";
+ weekday[4] = "Thursday";
+ weekday[5] = "Friday";
+ weekday[6] = "Saturday";
+
+ return weekday[this.getDay()];
 };
 
 function pad(number)
