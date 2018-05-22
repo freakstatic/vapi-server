080d17777615b6fe5f8ec8e1eeb24e0214b750ce
rcaceiro <r.caceiro@hotmail.com>
2018-05-22 23:41:23 +0100

    angular authentication

--- angular/src/app/auth/auth.service.ts
+++ angular/src/app/auth/auth.service.ts
@@ -1,49 +1,64 @@
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
+import {Socket} from 'ngx-socket-io';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {Http} from "@angular/http";
-import {Observable} from "rxjs/Observable";
-import {reject} from "q";
-
 
 @Injectable()
-export class AuthService {
-    private loggedIn = new BehaviorSubject<boolean>(true);
+export class AuthService
+{
+ private loggedIn = new BehaviorSubject<boolean>(true);
 
-    get isLoggedIn() {
-        return this.loggedIn.asObservable();
+ constructor(private router: Router, private http: HttpClient, private socket: Socket)
+ {
+ }
+
+ get isLoggedIn()
+ {
+  return this.loggedIn.asObservable();
+ }
+
+ login(username: string, password: string)
+ {
+  return new Promise((resolve, reject) =>
+  {
+   this.http.post('api/login', {username: username, password: password}).subscribe(() =>
+   {
+    this.loggedIn.next(true);
+    this.socket.emit('authenticate', {username: username, password: password});
+    resolve();
+   }, (errorResponse: HttpErrorResponse) =>
+   {
+    reject(errorResponse);
+   });
+  })
+ }
+
+
+ logout()
+ {
+  this.loggedIn.next(false);
+  this.router.navigate(['/login']);
+ }
+
+ checkLogin()
+ {
+  return new Promise((resolve, reject) =>
+  {
+   this.http.get('api/login/check').subscribe((response: any) =>
+   {
+    this.loggedIn.next(response.isLoggedIn);
+    if (!response.isLoggedIn)
+    {
+     this.socket.emit('authenticate');
     }
-
-    constructor(private router: Router, private http: HttpClient) {
-    }
-
-    login(username: string, password: string) {
-        return new Promise((resolve, reject) => {
-            this.http.post('api/login', {username: username, password: password}).subscribe(() => {
-                this.loggedIn.next(true);
-                resolve();
-            }, (errorResponse: HttpErrorResponse) => {
-                reject(errorResponse);
-            });
-        })
-    }
-
-
-    logout() {
-        this.loggedIn.next(false);
-        this.router.navigate(['/login']);
-    }
-
-    checkLogin() {
-        return new Promise((resolve, reject) => {
-            this.http.get('api/login/check').subscribe((response: any) => {
-                this.loggedIn.next(response.isLoggedIn);
-                resolve(response.isLoggedIn);
-            }, (error: HttpErrorResponse) => {
-                this.loggedIn.next(false);
-                resolve(false);
-            });
-        })
-    }
+    resolve(response.isLoggedIn);
+   }, (error: HttpErrorResponse) =>
+   {
+    this.loggedIn.next(false);
+    this.socket.emit('authenticate');
+    resolve(false);
+   });
+  })
+ }
 }
