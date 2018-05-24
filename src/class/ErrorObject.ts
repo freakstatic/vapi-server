69d55b61fa0ac6c8f39d8389f7f20480322d9745
rcaceiro <r.caceiro@hotmail.com>
2018-05-24 19:57:00 +0100

    new error constants New dependencies

--- src/class/ErrorObject.ts
+++ src/class/ErrorObject.ts
@@ -1,24 +1,27 @@
-export class ErrorObject {
-    private code: string;
-    private readonly message: string;
+export class ErrorObject
+{
+ static errorMessages = {
+  EMPTY_USERNAME: 'Empty username',
+  EMPTY_PASSWORD: 'Empty password',
+  EMPTY_USER_ID: 'Empty user id',
+  INVALID_USERNAME_OR_PASSWORD: 'Username and password combination not found',
+  MOTION_INVALID_SETTINGS: 'Invalid Motion settings'
+ };
+ static readonly EMPTY_USERNAME = 'EMPTY_USERNAME';
+ static readonly EMPTY_PASSWORD = 'EMPTY_PASSWORD';
+ static readonly EMPTY_USER_ID = 'EMPTY_ID';
+ static readonly INVALID_USERNAME_OR_PASSWORD = 'INVALID_USERNAME_OR_PASSWORD';
+ static readonly MOTION_INVALID_SETTINGS = 'MOTION_INVALID_SETTINGS';
+ private code: string;
+ private readonly message: string;
 
-    static errorMessages = {
-        EMPTY_USERNAME: 'Empty username',
-        EMPTY_PASSWORD: 'Empty password',
-        INVALID_USERNAME_OR_PASSWORD: 'Username and password combination not found',
-        MOTION_INVALID_SETTINGS: 'Invalid Motion settings'
-    };
-
-    static readonly EMPTY_USERNAME = 'EMPTY_USERNAME';
-    static readonly EMPTY_PASSWORD = 'EMPTY_PASSWORD';
-    static readonly INVALID_USERNAME_OR_PASSWORD = 'INVALID_USERNAME_OR_PASSWORD';
-    static readonly MOTION_INVALID_SETTINGS = 'MOTION_INVALID_SETTINGS';
-
-    constructor(code: string) {
-        this.message = ErrorObject.errorMessages[code];
-        if (!this.message){
-            throw new Error("Invalid error code");
-        }
-        this.code = code;
-    }
+ constructor(code: string)
+ {
+  this.message = ErrorObject.errorMessages[code];
+  if (!this.message)
+  {
+   throw new Error("Invalid error code");
+  }
+  this.code = code;
+ }
 }
