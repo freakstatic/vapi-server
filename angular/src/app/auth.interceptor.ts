5eaa7b9f77c0a89dca4f25582533a6caa669b018
rcaceiro <r.caceiro@hotmail.com>
2018-06-06 12:35:32 +0100

    added HTTPInterceptor to set authorization header

---
+++ angular/src/app/auth.interceptor.ts
@@ -0,0 +1,37 @@
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {CredentialsManagerService} from './auth/credentials.manager.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor
+{
+ constructor(private credentialsManager: CredentialsManagerService)
+ {}
+
+ intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>
+ {
+  if (req.url.match("./assets/i18n/([^][^].json)"))
+  {
+   return next.handle(req);
+  }
+  let authReq = req;
+  if (req.url.match("api/login"))
+  {
+   if (!this.credentialsManager.checkLogin())
+   {
+    authReq = req.clone({
+     setHeaders: {
+      Authorization: "Basic " + btoa(req.body.username + ':' + req.body.password)
+     },
+     body:null
+    })
+   }
+  }
+  else
+  {
+
+  }
+  return next.handle(authReq);
+ }
+}
