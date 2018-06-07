68699e1ab357f128a4dbb5c4a052d13426e4b09c
rcaceiro <r.caceiro@hotmail.com>
2018-06-07 18:43:02 +0100

    imports and unnecessary modules

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
@@ -9,24 +9,26 @@ import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
 import {DashboardService} from 'app/dashboard/dashboard.service';
-import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
-import {environment} from './../environments/environment';
+import {Socket, SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
+import {environment} from '../environments/environment';
 
 import {AppComponent} from './app.component';
 
 import {AppRoutingModule} from './app.routing';
+import {AuthInterceptor} from './auth.interceptor';
 import {AuthGuard} from './auth/auth.guard';
 import {AuthService} from './auth/auth.service';
+import {CredentialsManagerService} from './auth/credentials.manager.service';
 import {ComponentsModule} from './components/components.module';
 
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {IconsComponent} from './icons/icons.component';
 import {LoginComponent} from './login/login.component';
-import {MapsComponent} from './maps/maps.component';
 import {NotificationsComponent} from './notifications/notifications.component';
 import {SettingsComponent} from './settings/settings.component';
 import {SettingsService} from "./settings/settings.service";
 import {TableListComponent} from './table-list/table-list.component';
+import {TimelapsesService} from './timelapses/timelapses.service';
 import {TitleService} from "./title.service";
 import {TypographyComponent} from './typography/typography.component';
 import {UpgradeComponent} from './upgrade/upgrade.component';
@@ -52,7 +54,6 @@ const config: SocketIoConfig = {
   TableListComponent,
   TypographyComponent,
   IconsComponent,
-  MapsComponent,
   NotificationsComponent,
   UpgradeComponent,
   LoginComponent,
@@ -75,7 +76,7 @@ const config: SocketIoConfig = {
   }),
   SocketIoModule.forRoot(config)
  ],
- providers: [AuthService, AuthGuard, TitleService, SettingsService, DashboardService,TimelapsesService, CredentialsManagerService, {
+ providers: [AuthService, AuthGuard, TitleService, SettingsService, DashboardService, TimelapsesService, CredentialsManagerService, {
   provide: HTTP_INTERCEPTORS,
   useClass: AuthInterceptor,
   multi: true
