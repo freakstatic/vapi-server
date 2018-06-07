65296de146d748acb416b526b79d9944f9bf1f87
rcaceiro <r.caceiro@hotmail.com>
2018-06-07 18:44:13 +0100

    imports

--- angular/src/app/app.routing.ts
+++ angular/src/app/app.routing.ts
@@ -1,19 +1,14 @@
-import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {Routes, RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthGuard} from "./auth/auth.guard";
 
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {UserProfileComponent} from './user-profile/user-profile.component';
-import {TableListComponent} from './table-list/table-list.component';
-import {TypographyComponent} from './typography/typography.component';
-import {IconsComponent} from './icons/icons.component';
-import {MapsComponent} from './maps/maps.component';
-import {NotificationsComponent} from './notifications/notifications.component';
-import {UpgradeComponent} from './upgrade/upgrade.component';
 import {LoginComponent} from "./login/login.component";
-import {AuthGuard} from "./auth/auth.guard";
 import {SettingsComponent} from "./settings/settings.component";
+import {TimelapsesComponent} from './timelapses/timelapses.component';
+import {UserListComponent} from './user-list/user-list.component';
 
 const routes: Routes = [
  {path: 'login', component: LoginComponent, data: {title: 'Login'}},
