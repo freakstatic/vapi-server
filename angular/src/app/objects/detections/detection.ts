e6b150986bf091bd37886f1e48a8f8d386533328
rcaceiro <r.caceiro@hotmail.com>
2018-06-09 12:11:09 +0100

    new factory for detection object

--- angular/src/app/objects/detections/detection.ts
+++ angular/src/app/objects/detections/detection.ts
@@ -6,4 +6,13 @@ export class Detection
  date: Date;
  detectionObjects: DetectionObject[];
  numberOfDetections: number;
+
+ public static NewInstanceFromDetectionSocket(data:any):Detection
+ {
+  let detection=new Detection();
+  detection.id=data.id;
+  detection.numberOfDetections=data.numberOfDetections;
+  detection.date=new Date(data.date);
+  return detection;
+ }
 }
