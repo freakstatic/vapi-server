dd7c3a81dfd0ba40eb50a1338a3c7dd83838aea1
rcaceiro <r.caceiro@hotmail.com>
2018-06-09 12:11:43 +0100

    little fix when new detection arrives

--- angular/src/app/dashboard/dashboard.service.ts
+++ angular/src/app/dashboard/dashboard.service.ts
@@ -10,95 +10,113 @@ import {Observable} from 'rxjs/Observable';
 import './../../utils/date.extensions';
 
 @Injectable()
-export class DashboardService {
-    constructor(private router: Router, private http: HttpClient, private socket: Socket, private translate: TranslateService) {
-        this.socket.on('detection', data => {
-            if (data == null) {
-                return;
-            }
-            let detection: Detection = data;
-            let object = this._detectionChartLast7Weeks.getValue();
-
-            let index = object.sourceObjects.findIndex(element => {
-                return element.date.toISOString() === detection.date.toISOString();
-            });
-            console.log(index);
-            if (index < 0) {
-                if (object.sourceObjects[object.sourceObjects.length - 1].date >= detection.date) {
-                    return;
-                }
-                object.sourceObjects.push(detection);
-                object.labels.push('1');
-                object.series[0].push(0);
-                object.sourceObjects.shift();
-                object.labels.shift();
-                object.series[0].shift();
-                index = object.sourceObjects.length - 1;
-            }
-            object.series[0][index] += detection.numberOfDetections;
-            if (object.series[0][index] > object.max) {
-                object.max = object.series[0][index];
-            }
-            this._detectionChartLast7Weeks.next(object);
-        });
+export class DashboardService
+{
+ constructor(private router: Router, private http: HttpClient, private socket: Socket, private translate: TranslateService)
+ {
+  this.socket.on('detection', data =>
+  {
+   if (data == null)
+   {
+    return;
+   }
+   let detection = Detection.NewInstanceFromDetectionSocket(data);
+   let object = this._detectionChartLast7Weeks.getValue();
+   let index = object.sourceObjects.findIndex(element =>
+   {
+    return element.date.toDateISOString() === detection.date.toDateISOString();
+   });
+   if (index < 0)
+   {
+    if (object.sourceObjects[object.sourceObjects.length - 1].date >= detection.date)
+    {
+     return;
     }
+    object.sourceObjects.push(detection);
+    object.labels.push('1');
+    object.series[0].push(0);
+    object.sourceObjects.shift();
+    object.labels.shift();
+    object.series[0].shift();
+    index = object.sourceObjects.length - 1;
+   }
+   object.series[0][index] += detection.numberOfDetections;
+   if (object.series[0][index] > object.max)
+   {
+    object.max = object.series[0][index];
+   }
+   this._detectionChartLast7Weeks.next(object);
+  });
+ }
 
-    private _detectionChartLast7Weeks = new BehaviorSubject<ChartObject<Detection>>(new ChartObject<Detection>());
+ private _detectionChartLast7Weeks = new BehaviorSubject<ChartObject<Detection>>(new ChartObject<Detection>());
 
-    get detectionChartLast7Weeks(): Observable<ChartObject<Detection>> {
-        return this._detectionChartLast7Weeks.asObservable();
-    }
+ get detectionChartLast7Weeks(): Observable<ChartObject<Detection>>
+ {
+  return this._detectionChartLast7Weeks.asObservable();
+ }
 
-    public async initDetectionChartLast7Weeks() {
-        let detections = await this.getDetectionStatsLastWeek();
-        let serie = [];
-        let object = this._detectionChartLast7Weeks.getValue();
-        object.series.push(serie);
-        for (let detection of detections) {
-            let weekdayName = detection.date.getDayName();
-            this.translate.get(weekdayName.toUpperCase()).subscribe((res: string) => {
-                object.labels.push(res.substr(0, 1).toUpperCase());
-            });
-            // object.labels.push(detection.date.toDateISOString());
-            serie.push(detection.numberOfDetections);
-            if (detection.numberOfDetections > object.max) {
-                object.max = detection.numberOfDetections;
-            }
-            object.sourceObjects.push(detection);
-        }
-        this._detectionChartLast7Weeks.next(object);
-    }
+ public async initDetectionChartLast7Weeks()
+ {
+  let detections = await this.getDetectionStatsLastWeek();
+  let serie = [];
+  let object = this._detectionChartLast7Weeks.getValue();
+  object.series.push(serie);
+  for (let detection of detections)
+  {
+   let weekdayName = detection.date.getDayName();
+   this.translate.get(weekdayName.toUpperCase()).subscribe((res: string) =>
+   {
+    object.labels.push(res.substr(0, 1).toUpperCase());
+   });
+   // object.labels.push(detection.date.toDateISOString());
+   serie.push(detection.numberOfDetections);
+   if (detection.numberOfDetections > object.max)
+   {
+    object.max = detection.numberOfDetections;
+   }
+   object.sourceObjects.push(detection);
+  }
+  this._detectionChartLast7Weeks.next(object);
+ }
 
-    public async getDetectionStatsLastWeek(): Promise<Detection[]> {
-        let startDate = new Date();
-        startDate.setDate(startDate.getDate() - 6);
-        startDate.setHours(0, 0, 0, 0);
-        let endDate = new Date();
-        endDate.setHours(24, 0, 0, 0);
-        return this.getDetectionStats(startDate, endDate);
-    }
+ public async getDetectionStatsLastWeek(): Promise<Detection[]>
+ {
+  let startDate = new Date();
+  startDate.setDate(startDate.getDate() - 6);
+  startDate.setHours(0, 0, 0, 0);
+  let endDate = new Date();
+  endDate.setHours(24, 0, 0, 0);
+  return this.getDetectionStats(startDate, endDate);
+ }
 
-    public getDetectionStats(startDate: Date, endDate: Date): Promise<Detection[]> {
-        return new Promise<Detection[]>((resolve, reject) => {
-            this.http.get('api/stats/detection?startDate=' + startDate.toISOString() + '&endDate=' + endDate.toISOString())
-                .subscribe((response: any) => {
-                    let detections: Detection[] = [];
+ public getDetectionStats(startDate: Date, endDate: Date): Promise<Detection[]>
+ {
+  return new Promise<Detection[]>((resolve, reject) =>
+  {
+   this.http.get('api/stats/detection?startDate=' + startDate.toISOString() + '&endDate=' + endDate.toISOString())
+    .subscribe((response: any) =>
+    {
+     let detections: Detection[] = [];
 
-                    for (let dateString in response) {
-                        if (!response.hasOwnProperty(dateString)) {
-                            continue;
-                        }
-                        let detection = new Detection();
-                        detection.numberOfDetections = response[dateString];
-                        detection.detectionObjects = null;
-                        detection.id = null;
-                        detection.date = new Date(dateString);
-                        detections.push(detection);
-                    }
-                    resolve(detections);
-                }, (errorResponse: HttpErrorResponse) => {
-                    reject(errorResponse);
-                });
-        })
-    }
+     for (let dateString in response)
+     {
+      if (!response.hasOwnProperty(dateString))
+      {
+       continue;
+      }
+      let detection = new Detection();
+      detection.numberOfDetections = response[dateString];
+      detection.detectionObjects = null;
+      detection.id = null;
+      detection.date = new Date(dateString);
+      detections.push(detection);
+     }
+     resolve(detections);
+    }, (errorResponse: HttpErrorResponse) =>
+    {
+     reject(errorResponse);
+    });
+  })
+ }
 }
