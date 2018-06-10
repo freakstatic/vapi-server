a90af71e4e4cef9b62138562beae150d18272d88
rcaceiro <r.caceiro@hotmail.com>
2018-06-10 12:02:42 +0100

    query builder done for top 5

--- src/repository/DetectableObjectRepository.ts
+++ src/repository/DetectableObjectRepository.ts
@@ -1,15 +1,38 @@
-import {EntityRepository, getConnection, Repository} from "typeorm";
+import {EntityRepository, Repository, SelectQueryBuilder} from "typeorm";
 import {DetectableObject} from "../entity/DetectableObject";
-import {Detection} from "../entity/Detection";
+import {DetectionObject} from '../entity/DetectionObject';
 
 @EntityRepository(DetectableObject)
-export class DetectableObjectRepository  extends Repository<DetectableObject>{
+export class DetectableObjectRepository extends Repository<DetectableObject>
+{
+ public async findByName(name: string)
+ {
+  return await this.findOne({
+   name: name
+  });
+ }
 
-    async findByName(name: string) {
-        return await this.findOne({
-            name: name
-        })
+ public async getTop5(): Promise<DetectableObject[]>
+ {
+  let dbObjs = null;
 
-    }
+  try
+  {
+   dbObjs = await this.createQueryBuilder('detectable')
+    .select(['detectable.id', 'detectable.name', 'detection_object.numberoccurrences'])
+    .innerJoin((queryBuilder: SelectQueryBuilder<DetectionObject>) =>
+    {
+     return queryBuilder
+      .select(["COUNT(detection_obj.id) AS 'numberoccurrences'","detection_obj.object_id"])
+      .from(DetectionObject,'detection_obj')
+      .groupBy('detection_obj.object_id')
+    }, 'detection_object', 'detection_object.object_id=detectable.id')
+    .orderBy('detection_object.numberoccurrences', 'DESC')
+    .limit(5)
+    .getMany();
+  }
+  catch (e)
+  {}
+  return dbObjs;
+ }
 }
-
