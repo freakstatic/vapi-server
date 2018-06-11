da503a13d534fecf2436b42e2c1eedeb5dce09fc
rcaceiro <r.caceiro@hotmail.com>
2018-06-11 13:27:16 +0100

    ts lint

--- angular/src/app/app.routing.ts
+++ angular/src/app/app.routing.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
-import {CommonModule,} from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
-import {Routes, RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 
 import {AdminLayoutComponent} from './layouts/admin-layout/admin-layout.component';
-import {LoginComponent} from "./login/login.component";
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
     {
