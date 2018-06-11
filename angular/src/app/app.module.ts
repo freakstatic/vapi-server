f9a728861daef0fb61017a523cb20f5026f1a8ec
rcaceiro <r.caceiro@hotmail.com>
2018-06-11 16:05:03 +0100

    added new guard to providers

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,40 +4,28 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {RouterModule} from '@angular/router';
 
-
 import {AppRoutingModule} from './app.routing';
 import {ComponentsModule} from './components/components.module';
 
 import {AppComponent} from './app.component';
 
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {UserProfileComponent} from './user-profile/user-profile.component';
-import {TableListComponent} from './table-list/table-list.component';
-import {TypographyComponent} from './typography/typography.component';
-import {IconsComponent} from './icons/icons.component';
-import {MapsComponent} from './maps/maps.component';
-import {NotificationsComponent} from './notifications/notifications.component';
-import {UpgradeComponent} from './upgrade/upgrade.component';
-import {
-    AgmCoreModule
-} from '@agm/core';
 import {AdminLayoutComponent} from './layouts/admin-layout/admin-layout.component';
-import {AuthService} from "./auth/auth.service";
-import {DashboardService} from "./dashboard/dashboard.service";
-import {SettingsService} from "./settings/settings.service";
-import {AuthGuard} from "./auth/auth.guard";
-import {TitleService} from "./title.service";
-import {TimelapsesService} from "./timelapses/timelapses.service";
-import {CredentialsManagerService} from "./auth/credentials.manager.service";
-import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
-import {AuthInterceptor} from "./auth.interceptor";
-import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
-import {environment} from "../environments/environment";
-import {LoginComponent} from "./login/login.component";
+import {AuthService} from './auth/auth.service';
+import {DashboardService} from './dashboard/dashboard.service';
+import {SettingsService} from './settings/settings.service';
+import {AuthGuard} from './auth/auth.guard';
+import {TitleService} from './title.service';
+import {TimelapsesService} from './timelapses/timelapses.service';
+import {CredentialsManagerService} from './auth/credentials.manager.service';
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
+import {AuthInterceptor} from './auth.interceptor';
+import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
+import {environment} from '../environments/environment';
+import {LoginComponent} from './login/login.component';
 
-import {TranslateLoader, TranslateModule, TranslateService} from "@ngx-translate/core";
-import {TranslateHttpLoader} from "@ngx-translate/http-loader";
-
+import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {AuthLoginGuard} from './auth/auth.login.guard';
 
 export function createTranslateLoader(http: HttpClient) {
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -73,7 +61,7 @@ const config: SocketIoConfig = {
         AdminLayoutComponent,
         LoginComponent,
     ],
-    providers: [AuthService, AuthGuard, TitleService, SettingsService, DashboardService, TimelapsesService,
+    providers: [AuthService, AuthGuard, AuthLoginGuard, TitleService, SettingsService, DashboardService, TimelapsesService,
         CredentialsManagerService, {
             provide: HTTP_INTERCEPTORS,
             useClass: AuthInterceptor,
