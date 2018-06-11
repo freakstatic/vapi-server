b7943c2b8ad2148cac1b7be395467bbc9b32cb5b
rcaceiro <r.caceiro@hotmail.com>
2018-06-11 16:04:38 +0100

    added can active guard to login

--- angular/src/app/app.routing.ts
+++ angular/src/app/app.routing.ts
@@ -5,30 +5,35 @@ import {RouterModule, Routes} from '@angular/router';
 
 import {AdminLayoutComponent} from './layouts/admin-layout/admin-layout.component';
 import {LoginComponent} from './login/login.component';
+import {AuthGuard} from './auth/auth.guard';
+import {AuthLoginGuard} from './auth/auth.login.guard';
 
 const routes: Routes = [
-    {
-        path: '', component: LoginComponent, data: {title: 'Login'},
-        pathMatch: 'full',
-    },
-    {
-        path: '',
-        component: AdminLayoutComponent,
-        children: [
-            {
-                path: '',
-                loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
-            }]
-    }
+ {
+  path: '', component: LoginComponent, data: {title: 'Login'},
+  pathMatch: 'full',
+  canActivate: [AuthLoginGuard]
+ },
+ {
+  path: '',
+  component: AdminLayoutComponent,
+  children: [
+   {
+    path: '',
+    loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+   }],
+  canActivate: [AuthGuard]
+ }
 ];
 
 @NgModule({
-    imports: [
-        CommonModule,
-        BrowserModule,
-        RouterModule.forRoot(routes)
-    ],
-    exports: [],
+ imports: [
+  CommonModule,
+  BrowserModule,
+  RouterModule.forRoot(routes)
+ ],
+ exports: []
 })
-export class AppRoutingModule {
+export class AppRoutingModule
+{
 }
