110d42e95feb4d855d8132f08e7daab4472f30d0
rcaceiro <r.caceiro@hotmail.com>
2018-06-11 21:49:04 +0100

    fix remove minutes

--- src/repository/DetectionRepository.ts
+++ src/repository/DetectionRepository.ts
@@ -61,7 +61,7 @@ export class DetectionRepository extends Repository<Detection>
   try
   {
    dbObjs=await this.createQueryBuilder()
-    .select(['COUNT(id) AS numberOccurrences','DATE_FORMAT(date,\'%H:00\') AS time'])
+    .select(['COUNT(id) AS numberOccurrences','DATE_FORMAT(date,\'%H\') AS time'])
     .groupBy('DATE_FORMAT(date,\'%H\')')
     .getRawMany();
   }
