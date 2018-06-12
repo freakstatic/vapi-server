73ca8ba5e8bf6bbd8748a15f4d56af87116651d3
rcaceiro <r.caceiro@hotmail.com>
2018-06-12 18:45:37 +0100

    added joinColumn

--- src/entity/DetectionObject.ts
+++ src/entity/DetectionObject.ts
@@ -1,33 +1,35 @@
-import {Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToOne, PrimaryGeneratedColumn} from "typeorm";
-import {Detection} from "./Detection";
-import {DetectableObject} from "./DetectableObject";
+import {Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn} from 'typeorm';
+import {Detection} from './Detection';
+import {DetectableObject} from './DetectableObject';
 
 @Entity()
-export class DetectionObject{
-
-    @PrimaryGeneratedColumn()
-    id: number;
-
-    @Column()
-    probability: number;
-
-    @Column()
-    x: number;
-
-    @Column()
-    y: number;
-
-    @Column()
-    width: number;
-
-    @Column()
-    height: number;
-
-    @OneToOne(type => Detection)
-    @JoinColumn()
-    detection: Detection;
-
-    @ManyToOne(type => DetectableObject)
-    object: DetectableObject;
-
+export class DetectionObject
+{
+ 
+ @PrimaryGeneratedColumn()
+ id: number;
+ 
+ @Column()
+ probability: number;
+ 
+ @Column()
+ x: number;
+ 
+ @Column()
+ y: number;
+ 
+ @Column()
+ width: number;
+ 
+ @Column()
+ height: number;
+ 
+ @OneToOne(type => Detection)
+ @JoinColumn()
+ detection: Detection;
+ 
+ @ManyToOne(type => DetectableObject)
+ @JoinColumn()
+ object: DetectableObject;
+ 
 }
