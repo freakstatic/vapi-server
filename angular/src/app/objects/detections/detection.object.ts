adb19a34174acdb07f85d0364bedd1cd3981b701
rcaceiro <r.caceiro@hotmail.com>
2018-06-12 10:32:27 +0100

    DetectionObject adapted

--- angular/src/app/objects/detections/detection.object.ts
+++ angular/src/app/objects/detections/detection.object.ts
@@ -11,4 +11,30 @@ export class DetectionObject
  height: number;
  detection: Detection;
  object: DetectableObject;
+ 
+ static Instance(data: any, detection?: Detection): DetectionObject
+ {
+  if (!(data.hasOwnProperty('id') &&
+   data.hasOwnProperty('probability') &&
+   data.hasOwnProperty('x') &&
+   data.hasOwnProperty('y') &&
+   data.hasOwnProperty('width') &&
+   data.hasOwnProperty('height')))
+  {
+   return null;
+  }
+  const detectionObject=new DetectionObject();
+  detectionObject.id=data.id;
+  detectionObject.probability=data.probability;
+  detectionObject.x=data.x;
+  detectionObject.y=data.y;
+  detectionObject.width=data.width;
+  detectionObject.height=data.height;
+  detectionObject.detection=detection;
+  if (data.hasOwnProperty('object'))
+  {
+   detectionObject.object=DetectableObject.Instance(data.object);
+  }
+  return detectionObject;
+ }
 }
