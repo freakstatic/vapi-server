e54bfc7ac400cb3b13464e8e4064ba7bc7fa46ce
rcaceiro <r.caceiro@hotmail.com>
2018-06-12 10:38:51 +0100

    reindent

--- src/entity/DetectionEvent.ts
+++ src/entity/DetectionEvent.ts
@@ -1,23 +1,23 @@
-import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
-import {Detection} from "./Detection";
-import {DetectionObject} from "./DetectionObject";
+import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from 'typeorm';
+import {Detection} from './Detection';
 
 @Entity()
-export class DetectionEvent {
-    @PrimaryGeneratedColumn()
-    id: number;
-
-    @Column({
-        precision: 4
-    })
-    startDate: Date;
-
-    @Column({
-        precision: 4,
-        nullable: true
-    })
-    endDate: Date;
-
-    @OneToMany(type => Detection, detection => detection.event)
-    detections: Detection[];
+export class DetectionEvent
+{
+ @PrimaryGeneratedColumn()
+ id: number;
+ 
+ @Column({
+  precision: 4
+ })
+ startDate: Date;
+ 
+ @Column({
+  precision: 4,
+  nullable: true
+ })
+ endDate: Date;
+ 
+ @OneToMany(type => Detection, detection => detection.event)
+ detections: Detection[];
 }
