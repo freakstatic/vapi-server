a94fecae4f63de007017d838b333e7ed146271c2
rcaceiro <r.caceiro@hotmail.com>
2018-06-12 10:14:03 +0100

    new detection object dto

--- angular/src/app/objects/detections/detection.ts
+++ angular/src/app/objects/detections/detection.ts
@@ -6,13 +6,66 @@ export class Detection
  date: Date;
  detectionObjects: DetectionObject[];
  numberOfDetections: number;
-
- public static NewInstanceFromDetectionSocket(data:any):Detection
+ image: DetectionImage;
+ event: DetectionEvent;
+ 
+ public static Instance(data: any): Detection
  {
-  let detection=new Detection();
-  detection.id=data.id;
-  detection.numberOfDetections=data.numberOfDetections;
-  detection.date=new Date(data.date);
+  if (!data.hasOwnProperty('date'))
+  {
+   return null;
+  }
+  const detection = new Detection();
+  detection.date = new Date(data.date);
+  if (data.hasOwnProperty('id'))
+  {
+   detection.id = data.id;
+  }
+  if (data.hasOwnProperty('numberOfDetections'))
+  {
+   detection.numberOfDetections = data.numberOfDetections;
+  }
+  if (data.hasOwnProperty('detectionObjects'))
+  {
+   detection.detectionObjects = [];
+   if (Array.isArray(data.detectionObjects))
+   {
+    for (const detectionObject of data.detectionObjects)
+    {
+     const obj = DetectionObject.Instance(detectionObject);
+     if (obj !== null)
+     {
+      detection.detectionObjects.push(obj);
+     }
+    }
+   }
+   else
+   {
+    const detectionObject = DetectionObject.Instance(data.detectionObjects);
+    if (detectionObject !== null)
+    {
+     detection.detectionObjects.push(detectionObject);
+    }
+   }
+  }
+ 
+  if (data.hasOwnProperty('image'))
+  {
+   const detectionImage = DetectionImage.Instance(data.image);
+   if (detectionImage !== null)
+   {
+    detection.detectionObjects.push(detectionImage);
+   }
+  }
+ 
+  if (data.hasOwnProperty('event'))
+  {
+   const detectionEvent = DetectionEvent.Instance(data.image);
+   if (detectionEvent !== null)
+   {
+    detection.detectionObjects.push(detectionEvent);
+   }
+  }
   return detection;
  }
 }
