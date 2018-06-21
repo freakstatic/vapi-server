4739e4eda9f6d081fed91a5f863d8898cf9e3cbe
Ricardo Maltez <ricardomaltez@gmail.com>
2018-06-21 15:02:50 +0100

    Major cleaning

--- angular/src/app/app.routing.ts
+++ angular/src/app/app.routing.ts
@@ -9,31 +9,30 @@ import {AuthGuard} from './auth/auth.guard';
 import {AuthLoginGuard} from './auth/auth.login.guard';
 
 const routes: Routes = [
- {
-  path: '', component: LoginComponent, data: {title: 'Login'},
-  pathMatch: 'full',
-  canActivate: [AuthLoginGuard]
- },
- {
-  path: '',
-  component: AdminLayoutComponent,
-  children: [
-   {
-    path: '',
-    loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
-   }]
-     //,canActivate: [AuthGuard]
- }
+    {
+        path: '', component: LoginComponent, data: {title: 'Login'},
+        pathMatch: 'full',
+        canActivate: [AuthLoginGuard]
+    },
+    {
+        path: '',
+        component: AdminLayoutComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+            }],
+        canActivate: [AuthGuard]
+    }
 ];
 
 @NgModule({
- imports: [
-  CommonModule,
-  BrowserModule,
-  RouterModule.forRoot(routes)
- ],
- exports: []
+    imports: [
+        CommonModule,
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: []
 })
-export class AppRoutingModule
-{
+export class AppRoutingModule {
 }
--- angular/src/app/layouts/admin-layout/admin-layout.module.ts
+++ angular/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -4,11 +4,6 @@ import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AdminLayoutRoutes} from './admin-layout.routing';
 import {DashboardComponent} from '../../dashboard/dashboard.component';
-import {UserProfileComponent} from '../../user-profile/user-profile.component';
-import {TableListComponent} from '../../table-list/table-list.component';
-import {TypographyComponent} from '../../typography/typography.component';
-import {IconsComponent} from '../../icons/icons.component';
-import {UpgradeComponent} from '../../upgrade/upgrade.component';
 
 import {
     MatButtonModule, MatDatepickerModule, MatInputModule, MatRippleModule, MatTooltipModule,
@@ -66,17 +61,7 @@ export function createTranslateLoader(http: HttpClient) {
     ],
     declarations: [
         DashboardComponent,
-        UserProfileComponent,
-        TableListComponent,
-        TypographyComponent,
-        IconsComponent,
-        UpgradeComponent,
         DashboardComponent,
-        UserProfileComponent,
-        TableListComponent,
-        TypographyComponent,
-        IconsComponent,
-        UpgradeComponent,
         SettingsComponent,
         TimelapsesComponent,
         UserListComponent,
