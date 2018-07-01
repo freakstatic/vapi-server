c8007a7c64165c0063980acf7d2fc1f8ae03e218
rcaceiro <r.caceiro@hotmail.com>
2018-07-01 18:42:15 +0100

    User now implemented

---
+++ angular/src/app/objects/user.ts
@@ -0,0 +1,21 @@
+export class User
+{
+ public id: number;
+ public username: string;
+ public email: string;
+ public groupId: number;
+ 
+ public static Instance(data: any): User
+ {
+  if (!data.hasOwnProperty('id')||!data.hasOwnProperty('username')||!data.hasOwnProperty('email')||!data.hasOwnProperty('group_id'))
+  {
+   return null;
+  }
+  const user = new User();
+  user.id=data.id;
+  user.username=data.username;
+  user.email=data.email;
+  user.groupId=data.group_id;
+  return user;
+ }
+}
