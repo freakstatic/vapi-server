5a4e83f9b8d896ef87511f09a42177ef651de657
rcaceiro <r.caceiro@hotmail.com>
2018-07-02 23:51:33 +0100

    done

--- angular/src/app/user-list/user-list.component.ts
+++ angular/src/app/user-list/user-list.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {User} from '../objects/user';
+import {UserListService} from './user-list.service';
+import {Group} from '../objects/group';
+import {MatDialog, MatDialogRef} from '@angular/material';
+import {UserDetailsModalComponent} from '../user-details-modal/user-details-modal.component';
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+ selector: 'app-user-list',
+ templateUrl: './user-list.component.html',
+ styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class UserListComponent implements OnInit
+{
+ public users:Observable<User[]>;
+ public groups:Observable<Group[]>;
+ 
+ constructor(private userListService:UserListService,private dialog:MatDialog)
+ {
+  this.users=this.userListService.users;
+  this.groups=this.userListService.groups;
+ }
+ 
+ ngOnInit()
+ {
+  this.userListService.getUsers();
+  this.userListService.getGroups();
+ }
+ 
+ public addNewUser()
+ {
+  const dialogRef=this.openUserDialog(null);
+  dialogRef.afterClosed().subscribe((result:User) => {
+   this.userListService.postUser(result);
+  });
+ }
+ 
+ public updateUser(user:User)
+ {
+  const userClone:User=JSON.parse(JSON.stringify(user));
+  const dialogRef=this.openUserDialog(userClone);
+  dialogRef.afterClosed().subscribe((result:User) => {
+   this.userListService.putUser(result);
+  });
+ }
+ 
+ public deleteUser(user:User)
+ {
+  this.userListService.deleteUser(user);
+ }
+ 
+ private openUserDialog(data?:User):MatDialogRef<UserDetailsModalComponent>
+ {
+  return this.dialog.open(UserDetailsModalComponent, {
+   width: '750px',
+   height: '550px',
+   data:data
+  });
+ }
 }
