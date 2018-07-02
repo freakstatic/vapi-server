7a6ed26441653cee4711f3cb711d6a90defd05e2
rcaceiro <r.caceiro@hotmail.com>
2018-07-02 14:57:46 +0100

    details modal done almost

--- angular/src/app/user-details-modal/user-details-modal.component.ts
+++ angular/src/app/user-details-modal/user-details-modal.component.ts
@@ -1,6 +1,10 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {User} from '../objects/user';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {FormControl, Validators} from '@angular/forms';
+import {UserListService} from '../user-list/user-list.service';
+import {Observable} from 'rxjs/Observable';
+import {Group} from '../objects/group';
 
 @Component({
  selector: 'app-user-details-modal',
@@ -9,15 +13,44 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 })
 export class UserDetailsModalComponent implements OnInit
 {
- constructor(public dialogRef: MatDialogRef<UserDetailsModalComponent>, @Inject(MAT_DIALOG_DATA) public user: User)
+ public groupID:string;
+ public groups:Observable<Group[]>;
+ private emailValidator:FormControl;
+ 
+ constructor(public dialogRef: MatDialogRef<UserDetailsModalComponent>,private userService:UserListService, @Inject(MAT_DIALOG_DATA) public user: User)
  {
-  if(this.user===undefined||this.user===null)
+  if (this.user === undefined || this.user === null)
   {
-   this.user=new User();
+   this.user = new User();
   }
+  if(this.user.groupId===undefined||this.user.groupId===null)
+  {
+   this.groupID='0';
+  }
+  else
+  {
+   this.groupID=this.user.groupId.toString(10);
+  }
+  this.groups=this.userService.groups;
+  this.emailValidator=new FormControl(this.user.email,[Validators.email]);
  }
  
  ngOnInit()
  {
+ }
+ 
+ public closeModalOnSave()
+ {
+  if(this.emailValidator.invalid)
+  {
+   return;
+  }
+  this.user.groupId=parseInt(this.groupID,10);
+  this.dialogRef.close(this.user);
+ }
+ 
+ getEmailErrorMessage()
+ {
+  return this.emailValidator.hasError('required') ? 'Email is invalid' :'';
  }
 }
