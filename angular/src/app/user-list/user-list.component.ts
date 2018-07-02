ecc12b2c73140102986efa218802621fbd1409dd
rcaceiro <r.caceiro@hotmail.com>
2018-07-03 00:18:38 +0100

    notification done

--- angular/src/app/user-list/user-list.component.ts
+++ angular/src/app/user-list/user-list.component.ts
@@ -5,6 +5,8 @@ import {UserListService} from './user-list.service';
 import {Group} from '../objects/group';
 import {MatDialog, MatDialogRef} from '@angular/material';
 import {UserDetailsModalComponent} from '../user-details-modal/user-details-modal.component';
+import {TranslateService} from '@ngx-translate/core';
+import {NavbarComponent} from '../components/navbar/navbar.component';
 
 @Component({
  selector: 'app-user-list',
@@ -13,13 +15,13 @@ import {UserDetailsModalComponent} from '../user-details-modal/user-details-moda
 })
 export class UserListComponent implements OnInit
 {
- public users:Observable<User[]>;
- public groups:Observable<Group[]>;
+ public users: Observable<User[]>;
+ public groups: Observable<Group[]>;
  
- constructor(private userListService:UserListService,private dialog:MatDialog)
+ constructor(private userListService: UserListService, private translateService: TranslateService, private dialog: MatDialog)
  {
-  this.users=this.userListService.users;
-  this.groups=this.userListService.groups;
+  this.users = this.userListService.users;
+  this.groups = this.userListService.groups;
  }
  
  ngOnInit()
@@ -30,42 +32,86 @@ export class UserListComponent implements OnInit
  
  public addNewUser()
  {
-  const dialogRef=this.openUserDialog(null);
-  dialogRef.afterClosed().subscribe((result:User|boolean|undefined) => {
-   if(result===undefined||result==null||!result)
+  const dialogRef = this.openUserDialog(null);
+  dialogRef.afterClosed().subscribe((result: User | boolean | undefined) =>
+  {
+   if (result === undefined || result == null || !result)
    {
     return;
    }
-   const aUser=result as User;
-   this.userListService.postUser(aUser);
+   const aUser = result as User;
+   this.userListService.postUser(aUser)
+    .then(() =>
+    {
+     this.translateService.get('USER_CREATED').subscribe((res: string) =>
+     {
+      NavbarComponent.showMessage(res);
+     });
+    })
+    .catch((error)=>
+    {
+     this.translateService.get('USER_NOT_CREATED').subscribe((res: string) =>
+     {
+      NavbarComponent.showMessage(res);
+     });
+    });
   });
  }
  
- public updateUser(user:User)
+ public updateUser(user: User)
  {
-  const userClone:User=JSON.parse(JSON.stringify(user));
-  const dialogRef=this.openUserDialog(userClone);
-  dialogRef.afterClosed().subscribe((result:User|boolean|undefined) => {
-   if(result===undefined||result==null||!result)
+  const userClone: User = JSON.parse(JSON.stringify(user));
+  const dialogRef = this.openUserDialog(userClone);
+  dialogRef.afterClosed().subscribe((result: User | boolean | undefined) =>
+  {
+   if (result === undefined || result == null || !result)
    {
     return;
    }
-   const aUser=result as User;
-   this.userListService.putUser(aUser);
+   const aUser = result as User;
+   this.userListService.putUser(aUser)
+    .then(() =>
+    {
+     this.translateService.get('USER_UPDATED').subscribe((res: string) =>
+     {
+      NavbarComponent.showMessage(res);
+     });
+    })
+    .catch((error)=>
+    {
+     this.translateService.get('USER_NOT_UPDATED').subscribe((res: string) =>
+     {
+      NavbarComponent.showMessage(res);
+     });
+    });
   });
  }
  
- public deleteUser(user:User)
+ public deleteUser(user: User)
  {
-  this.userListService.deleteUser(user);
+  this.userListService.deleteUser(user)
+   .then(() =>
+   {
+    this.translateService.get('USER_DELETED').subscribe((res: string) =>
+    {
+     NavbarComponent.showMessage(res);
+    });
+   })
+   .catch((error)=>
+   {
+    this.translateService.get('USER_NOT_DELETED').subscribe((res: string) =>
+    {
+     NavbarComponent.showMessage(res);
+    });
+   });
  }
  
- private openUserDialog(data?:User):MatDialogRef<UserDetailsModalComponent>
+ private openUserDialog(data?: User): MatDialogRef<UserDetailsModalComponent>
  {
   return this.dialog.open(UserDetailsModalComponent, {
    width: '750px',
    height: '550px',
-   data:data
+   data: data
   });
  }
 }
