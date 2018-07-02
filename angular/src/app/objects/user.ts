c5829243b545f0e3ce4ce0eb03931fd06b9ed0dc
rcaceiro <r.caceiro@hotmail.com>
2018-07-02 21:59:38 +0100

    fix

--- angular/src/app/objects/user.ts
+++ angular/src/app/objects/user.ts
@@ -4,20 +4,20 @@ export class User
  public username: string;
  public email: string;
  public groupId: number;
- public password:string;
+ public password: string;
  
  public static Instance(data: any): User
  {
-  if (!data.hasOwnProperty('id')||!data.hasOwnProperty('username')||!data.hasOwnProperty('email')||!data.hasOwnProperty('group_id'))
+  if (!data.hasOwnProperty('id') || !data.hasOwnProperty('username') || !data.hasOwnProperty('email') || !data.hasOwnProperty('group_id'))
   {
    return null;
   }
   const user = new User();
-  user.id=data.id;
-  user.username=data.username;
-  user.email=data.email;
-  user.groupId=data.group_id;
-  user.password=null;
+  user.id = data.id;
+  user.username = data.username;
+  user.email = data.email;
+  user.groupId = data.group_id;
+  user.password = null;
   return user;
  }
 }
